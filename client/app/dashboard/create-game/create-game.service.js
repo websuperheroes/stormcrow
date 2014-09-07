956329647614dbc3363339c42dc3e1eefb230c7c
Bård N Hovde <bnhovde@gmail.com>
2014-09-07 14:31:10 +0100

    Remove unneeded service

--- client/app/dashboard/create-game/create-game.service.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('stormcrowApp')
-    .factory('CreateGame', function($resource) {
-      return $resource('/api/games-api/:dest', {}, {
-          createGame: {method: 'POST', params: {dest: 'createGame'}, isArray: false},
-        });
-  })
